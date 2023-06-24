c0b89af9ee8731d49774236c410db57f0f31dcff
felpofo <felipepitolpuhl@gmail.com>
2023-06-24 20:07:54 -0300

    chore: project setup

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+export default {
+  root: true,
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:svelte/recommended",
+    "plugin:editorconfig/all",
+  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "editorconfig"],
+  parserOptions: {
+    sourceType: "module",
+    ecmaVersion: 2020,
+    extraFileExtensions: [".svelte"],
+  },
+  env: {
+    browser: true,
+    es2017: true,
+    node: true,
+  },
+  overrides: [
+    {
+      files: ["*.svelte"],
+      parser: "svelte-eslint-parser",
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+      },
+    },
+  ],
+  rules: {
+    quotes: ["error", "double"],
+  },
+};
